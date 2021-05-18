b25ea4080893e6f850a708389f9391c5bb5e342b
vicolaspetru <vicolaspetru@gmail.com>
2021-05-18 15:29:36 +0300

    v1.1.1

--- customizer-reset.php
+++ customizer-reset.php
@@ -3,7 +3,7 @@
  * Plugin Name: Customizer Reset
  * Plugin URI: http://wordpress.org/plugins/customizer-reset/
  * Description: Reset theme customizations (theme_mods) made via WordPress Customizer
- * Version: 1.1.0
+ * Version: 1.1.1
  * Author: WPZOOM
  * Author URI: http://wpzoom.com/
  * Text Domain: customizer-reset
